bece5c021366083322fd173c6736c0f43504ec67
Unathi Chonco <choncou@gmail.com>
2016-03-16 22:23:13 -0700

    Initial Commit

---
+++ CryptoBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CryptoBar
+//
+//  Created by Unathi Chonco on 2016/03/16.
+//  Copyright © 2016 Unathi Chonco. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
