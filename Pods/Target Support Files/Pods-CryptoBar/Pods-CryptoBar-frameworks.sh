99cd053ed574bbc4d831fc1ecfe0345c6d045ac1
Unathi Chonco <choncou@gmail.com>
2016-04-17 17:02:11 -0700

    add Realm pod again

--- Pods/Target Support Files/Pods-CryptoBar/Pods-CryptoBar-frameworks.sh
+++ Pods/Target Support Files/Pods-CryptoBar/Pods-CryptoBar-frameworks.sh
@@ -86,10 +86,14 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-CryptoBar/Alamofire.framework"
   install_framework "Pods-CryptoBar/Gloss.framework"
+  install_framework "Pods-CryptoBar/Realm.framework"
+  install_framework "Pods-CryptoBar/RealmSwift.framework"
   install_framework "Pods-CryptoBar/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-CryptoBar/Alamofire.framework"
   install_framework "Pods-CryptoBar/Gloss.framework"
+  install_framework "Pods-CryptoBar/Realm.framework"
+  install_framework "Pods-CryptoBar/RealmSwift.framework"
   install_framework "Pods-CryptoBar/SwiftyJSON.framework"
 fi
